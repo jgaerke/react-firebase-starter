8849ccf5538d0d435d06a20eee1985e6a98c22bc
GAERKE, JEREMY <jeremy.gaerke@kroger.com>
2016-03-09 17:07:56 -0500

    baseline, functional build.

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
+
 import { App, Home, Foo, Bar } from './components'
 import configureStore from './store'
-import styles from './style/site.css'
+import site from './styles/application.less'
 
 var store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store)
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+//const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
-module.exports = {
-  entry: './index.js',
+var webPackConfig = {
+  //devtool: 'source-map',
+  entry: [
+    './index.js'
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -13,30 +17,59 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel'
+        loader: 'babel',
+        exclude: '/node_modules/'
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
+        loader: 'html-loader',
+        exclude: '/node_modules/'
       },
       {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        test: /\.less$/,
+        loader: "style!css!less",
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192',
+        exclude: '/node_modules/'
       }
-    ],
+    ]
   },
-
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: process.env.NODE_ENV === 'production'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './template/index.html',
-      inject: 'body',
-      hash: true
-    })
-  ]
+  plugins: []
 }
+
+if (process.env.NODE_ENV === 'production') {
+  webPackConfig.plugins.push(new CleanWebpackPlugin(['dist']))
+  webPackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin())
+  webPackConfig.plugins.push(new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: './templates/index.html',
+    inject: 'body',
+    hash: true
+  }))
+}
+
+module.exports = webPackConfig;
 
